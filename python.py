59fdf3cb63daabb3b516f67043c2d2cc89a789c4
justgasland <abdulgaffarsonola@gmail.com>
2025-01-05 07:48:40 +0100

    first commit

--- python.py
+++ python.py
@@ -2,3 +2,25 @@
 name = input("Enter name: ")
 print(name)
 
+# Add two numbers
+a = 5
+b = 3
+print("Addition:", a + b) 
+
+# Subtract two numbers
+print("Subtraction:", a - b)  
+
+# Multiply two numbers
+print("Multiplication:", a * b)  
+
+# Divide two numbers
+print("Division:", a / b)  
+
+
+# Check if a number is positive or negative
+number = int(input("Enter a number: "))
+
+if number > 0:
+    print("The number is positive.")
+else:
+    print("The number is negative.")
